260768a58301c9ec7c014d12f2ea515bc495afab
clearleo <g567zp@gmail.com>
2024-10-13 16:56:11 -0500

    Ehrm.. documentation updates before releasing lmao

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A library made for easier command line argument management
 //!
 //! Initially it was supposed to be a boilerplate module for my own command line utilities but it looked pretty easy to make a library
-//! You might want to write this yourself for your usecase inside a module, this is just a general library that I've made for my OWN usecases.
+//! You might want to write this yourself for your usecase if my library doesn't help you.
 //! 
 //! At the moment I haven't tested this library fully, please beware of possible errors even though there shouldn't be any.
 
